02131215d201e879ad1d1375c00a94182d6e6a32
fsi-katami <openrtm@openrtm.org>
2009-03-13 07:34:51 +0000

    For OMG RTC 1.0
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0_0@62 9a6ac7ee-f497-465d-b832-7029f4913c1d

---
+++ jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/util/PortServiceListHolderFactory.java
@@ -0,0 +1,22 @@
+package jp.go.aist.rtm.RTC.util;
+
+import RTC.PortService;
+import RTC.PortServiceListHolder;
+
+public class PortServiceListHolderFactory {
+
+    public static PortServiceListHolder create() {
+        
+        return new PortServiceListHolder(new PortService[0]);
+    }
+
+    public static PortServiceListHolder clone(PortServiceListHolder rhs) {
+        
+        PortService[] ports = new PortService[rhs.value.length];
+        for (int i = 0; i < ports.length; i++) {
+            ports[i] = rhs.value[i];
+        }
+        
+        return new PortServiceListHolder(ports);
+    }
+}
