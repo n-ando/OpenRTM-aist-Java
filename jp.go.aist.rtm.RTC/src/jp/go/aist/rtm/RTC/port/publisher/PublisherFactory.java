ed68445dc28e46b7801abae5b925c5899e674a6d
fsi-katami <openrtm@openrtm.org>
2009-10-06 02:26:52 +0000

    The unit test of PublisherPeriodic class and PublisherNew class were done, and some bugs were fixed.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0_0@139 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/port/publisher/PublisherFactory.java
+++ jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/port/publisher/PublisherFactory.java
@@ -22,9 +22,8 @@ public class PublisherFactory {
      * 情報が設定されているPropertyオブジェクト
      * @return 適切なPublisherを生成できた場合は、そのオブジェクトを返します。さもなくばnullを返します。
      */
-/*
     public PublisherBase create(InPortConsumer consumer, Properties property) {
-        
+/*        
         String pub_type = property.getProperty("dataport.subscription_type", "New");
         
         if (pub_type.equals("New")) {
@@ -36,10 +35,10 @@ public class PublisherFactory {
         } else if (pub_type.equals("Flush")) {
             return new PublisherFlush(consumer, property);
         }
+*/
         
         return null;
     }
-*/
 
     /**
      * <p>指定されたPublisherを破棄します。</p>
