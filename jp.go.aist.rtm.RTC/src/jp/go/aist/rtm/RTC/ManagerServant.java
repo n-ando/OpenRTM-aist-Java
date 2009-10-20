09a59bbf5e61847143a8de02303a3ede55d994e2
hakuta <openrtm@openrtm.org>
2009-10-20 07:02:39 +0000

    Fixed problem in ManagerServant Class and Test program was updated.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0_0@162 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/ManagerServant.java
+++ jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/ManagerServant.java
@@ -36,8 +36,6 @@ public class ManagerServant extends ManagerPOA {
      */
     public ManagerServant() {
         m_mgr = jp.go.aist.rtm.RTC.Manager.instance();
-//        this.m_objref = RTM.ManagerHelper.narrow(this._this()._duplicate());
-//        this.m_objref = RTM.ManagerHelper.narrow(this._this());
         this.m_objref = this._this();
     }
 
@@ -51,12 +49,10 @@ public class ManagerServant extends ManagerPOA {
         if (this.m_objref == null) {
             try {
                 this.m_objref = ManagerHelper.narrow(POAUtil.getRef(this));
-                
             } catch (Exception e) {
                 throw new IllegalStateException(e);
             }
         }
-
         return this.m_objref;
     }
 
@@ -71,7 +67,7 @@ public class ManagerServant extends ManagerPOA {
         m_mgr.load(pathname, initfunc);
         return ReturnCode_t.RTC_OK;
     }
-  
+
     /**
      * <p> unload_module </p>
      *
@@ -85,70 +81,73 @@ public class ManagerServant extends ManagerPOA {
       }
       return ReturnCode_t.RTC_OK;
     }
-  
+
     /**
      * <p> get_loadable_modules </p>
      *
      * @return RTM.ModuleProfile[]
      */
     public RTM.ModuleProfile[] get_loadable_modules() {
-        ModuleProfileListHolder cprof = new ModuleProfileListHolder();
-
-        NVListHolder nvlist = new NVListHolder();
+        _SDOPackage.NVListHolder nvlist = new _SDOPackage.NVListHolder();
         Vector<Properties> prof = m_mgr.getLoadableModules();
-        cprof.value = new ModuleProfile[prof.size()];
+        ModuleProfile[] cprof = new ModuleProfile[prof.size()];
+        String[] strs = new String[2];
 
-//zxc        cprof.length(prof.size());
-System.out.println( "ManagerServant::get_loadable_modules--000--prof.size():"+ prof.size());
-        for (int i=0; i < prof.size(); ++i) {
-//            NVUtil.copyFromProperties(cprof.value[i].properties,
-//                                       prof.elementAt(i));
-            NVUtil.copyFromProperties(nvlist, prof.elementAt(i));
-	    cprof.value[i].properties = nvlist.value;
-	}
-        return cprof.value;
+        for (int i=0, len=prof.size(); i < len; ++i) {
+            strs[0] = prof.elementAt(i).getName();
+            strs[1] = prof.elementAt(i).getValue();
+            Properties prop = new Properties(strs);
+            NVUtil.copyFromProperties(nvlist, prop);
+
+            ModuleProfile cprof2 = new ModuleProfile();
+            cprof2.properties =  new _SDOPackage.NameValue [nvlist.value.length];
+            cprof2.properties =  nvlist.value;
+            cprof[i] = cprof2;
+        }
+
+        return cprof;
     }
-  
+
     /**
      * <p> get_loaded_modules </p>
      *
      * @return RTM.ModuleProfile[]
      */
     public RTM.ModuleProfile[] get_loaded_modules() {
-        ModuleProfileListHolder cprof = new ModuleProfileListHolder();
-  
-        NVListHolder nvlist = new NVListHolder();
+        _SDOPackage.NVListHolder nvlist = new _SDOPackage.NVListHolder();
         Vector<Properties> prof = m_mgr.getLoadedModules();
-        cprof.value = new ModuleProfile[prof.size()];
-    
-//zxc        cprof.length(prof.size());
-System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ prof.size());
-        for (int i=0; i < prof.size(); ++i) {
-//            NVUtil.copyFromProperties(cprof.value[i].properties,
-//                                       prof.elementAt(i));
-            NVUtil.copyFromProperties(nvlist, prof.elementAt(i));
-	    cprof.value[i].properties = nvlist.value;
+        ModuleProfile[] cprof = new ModuleProfile[prof.size()];
+        String[] strs = new String[2];
+
+        for (int i=0, len=prof.size(); i < len; ++i) {
+            strs[0] = prof.elementAt(i).getName();
+            strs[1] = prof.elementAt(i).getValue();
+            Properties prop = new Properties(strs);
+            NVUtil.copyFromProperties(nvlist, prop);
+
+            ModuleProfile cprof2 = new ModuleProfile();
+            cprof2.properties =  new _SDOPackage.NameValue [nvlist.value.length];
+            cprof2.properties =  nvlist.value;
+            cprof[i] = cprof2;
         }
-        return cprof.value;
+
+        return cprof;
     }
-  
+
     /**
      * <p> get_factory_profiles </p>
      *
      * @return RTM.ModuleProfile[]
      */
     public RTM.ModuleProfile[] get_factory_profiles() {
-
         _SDOPackage.NVListHolder nvlist = new _SDOPackage.NVListHolder();
-        nvlist.value = new _SDOPackage.NameValue[0];
-
         Vector<Properties> prof = m_mgr.getFactoryProfiles();
         ModuleProfile[] cprof = new ModuleProfile[prof.size()];
-//zxc       cprof.length(prof.size());
-        
+
         for (int i=0, len=prof.size(); i < len; ++i) {
-//            NVUtil.copyFromProperties(cprof[i].properties,prof.elementAt(i));
-            NVUtil.copyFromProperties(nvlist, prof.elementAt(i));
+            Properties prop = prof.elementAt(i);
+            NVUtil.copyFromProperties(nvlist, prop);
+
             ModuleProfile cprof2 = new ModuleProfile();
             cprof2.properties =  new _SDOPackage.NameValue [nvlist.value.length];
             cprof2.properties =  nvlist.value;
@@ -166,15 +165,14 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
      *
      */
     public RTC.RTObject create_component(final String module_name) {
-        System.out.println( "Manager::create_component: "  + module_name );
         RTObject_impl rtc = m_mgr.createComponent(module_name);
         if (rtc == null) {
-            System.out.println( "RTC not found: "  + module_name );
+//            System.err.println( "ManagerServant.create_component() RTC not found: "  + module_name );
             return null;
         }
         return rtc.getObjRef();
     }
-  
+
     /**
      * <p> delete_component </p>
      *
@@ -186,7 +184,7 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
         m_mgr.deleteComponent(instance_name);
         return ReturnCode_t.RTC_OK;
     }
-  
+
     /**
      * <p> get_components </p>
      *
@@ -196,10 +194,9 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
         Vector<RTObject_impl> rtcs = m_mgr.getComponents();
         RTCListHolder crtcs = new RTCListHolder();
         crtcs.value = new RTObject[rtcs.size()];
-//zxc        crtcs.length(rtcs.size());
+
         for (int i=0, len=rtcs.size(); i < len; ++i) {
-	    crtcs.value[i] = rtcs.elementAt(i).getObjRef();
-//            CORBA_SeqUtil.push_back(crtcs, rtcs.elementAt(i).getObjRef());
+            crtcs.value[i] = rtcs.elementAt(i).getObjRef();
         }
         return crtcs.value;
     }
@@ -210,19 +207,16 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
      * @return RTC.ComponentProfile[]
      */
     public RTC.ComponentProfile[] get_component_profiles() {
-
         ComponentProfileListHolder cprofs = new ComponentProfileListHolder();
-
         Vector<RTObject_impl> rtcs = m_mgr.getComponents();
         cprofs.value = new ComponentProfile[rtcs.size()];
 
         for (int i=0, len=rtcs.size(); i < len; ++i) {
             cprofs.value[i] = rtcs.elementAt(i).get_component_profile();
-	    //            CORBA_SeqUtil.push_back(cprofs, rtcs.elementAt(i).get_component_profile());
         }
         return cprofs.value;
     }
-  
+
     /**
      * <p> get_profile </p>
      *
@@ -231,14 +225,12 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
     public RTM.ManagerProfile get_profile() {
         NVListHolder nvlist = new NVListHolder();
         ManagerProfile prof = new ManagerProfile();
-//zxc        NVUtil.copyFromProperties(prof.properties,
-//                                  (m_mgr.getConfig().getNode("manager")));
         NVUtil.copyFromProperties(nvlist,
                                  (m_mgr.getConfig().getNode("manager")));
-	prof.properties = nvlist.value;
+        prof.properties = nvlist.value;
         return prof;
     }
-  
+
     /**
      * <p> get_configuration </p>
      * 
@@ -249,7 +241,7 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
         NVUtil.copyFromProperties(nvlist, m_mgr.getConfig());
         return nvlist.value;
     }
-  
+
     /**
      * <p> set_configuration </p>
      *
@@ -261,7 +253,7 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
         m_mgr.getConfig().setProperty(name, value);
         return ReturnCode_t.RTC_OK;
     }
-  
+
     /**
      * <p> get_owner </p>
      *
@@ -270,7 +262,7 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
     public RTM.Manager get_owner() {
         return null;
     }
-  
+
     /**
      * <p> set_owner </p>
      *
@@ -281,7 +273,7 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
     public RTM.Manager set_owner(RTM.Manager mgr) {
         return null;
     }
-  
+
     /**
      * <p> get_child </p>
      *
@@ -290,14 +282,14 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
     public RTM.Manager get_child() {
         return null;
     }
-  
+
     /**
      * <p> set_child </p>
      */
     public RTM.Manager set_child(RTM.Manager mgr) {
         return null;
     }
-  
+
     /**
      * <p> fork </p>
      */
@@ -305,7 +297,7 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
     //    m_mgr.fork();
         return ReturnCode_t.RTC_OK;
     }
-  
+
     /**
      * <p> shutdown </p>
      */
@@ -313,7 +305,7 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
         m_mgr.terminate();
         return ReturnCode_t.RTC_OK;
     }
-  
+
     /**
      * <p> restart </p>
      */
@@ -321,19 +313,18 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
     //    m_mgr.restart();
         return ReturnCode_t.RTC_OK;
     }
-  
+
     /**
      * <p> get_service </p>
      */
     public org.omg.CORBA.Object get_service(final String name) {
         return null;
     }
-  
+
     /**
      * <p> getObjRef </p>
      */
     public RTM.Manager getObjRef() {
-//        return (RTM.Manager)m_objref._this()._duplicate();
         return (RTM.Manager)m_objref;
     }
 
@@ -341,17 +332,17 @@ System.out.println( "ManagerServant::get_loaded_modules--000--prof.size():"+ pro
      * <p> setObjRef </p>
      */
     public void setObjRef(final RTM.Manager rtobj) {
-
         m_objref = rtobj;
     }
+
     /**
      * <p></p>
      */
     private jp.go.aist.rtm.RTC.Manager m_mgr;
+
     /**
      * <p></p>
      */
     private RTM.Manager m_objref;
 
 }
-
