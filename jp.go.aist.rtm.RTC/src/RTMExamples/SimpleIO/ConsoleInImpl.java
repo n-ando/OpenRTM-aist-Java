982ef3f1b6b58d4a47e1e5486522a43b036e3798
fsi-katami <openrtm@openrtm.org>
2009-10-28 05:18:08 +0000

    Removed println() for debug.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0_0@182 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/RTMExamples/SimpleIO/ConsoleInImpl.java
+++ jp.go.aist.rtm.RTC/src/RTMExamples/SimpleIO/ConsoleInImpl.java
@@ -101,9 +101,6 @@ public class ConsoleInImpl  extends DataFlowComponentBase {
         }
         System.out.println("Sending to subscriber: "  + m_out_val.data);
         m_outOut.write();
-RTC.Time tm = new RTC.Time(0,0);
-RTC.TimedLong tl = new RTC.TimedLong(tm,108);
-m_outOut.write(tl);
 
         return ReturnCode_t.RTC_OK;
     }
