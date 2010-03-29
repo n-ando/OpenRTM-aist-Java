1f438f6e29b3724345ced89aec34431b923257fe
hakuta <openrtm@openrtm.org>
2010-03-29 06:19:58 +0000

    Installer build scripts have been added.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0_0@304 9a6ac7ee-f497-465d-b832-7029f4913c1d

---
+++ jp.go.aist.rtm.RTC/installer/win32/OpenRTMjavawxs.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+#
+# @brief WiX wxs file generator for OpenRTM-aist-Java
+# @date $Date$
+# @author Norkai Ando <n-ando@aist.go.jp>
+#
+# Copyright (C) 2010
+#     Noriaki Ando
+#     Intelligent Systems Research Institute,
+#     National Institute of
+#         Advanced Industrial Science and Technology (AIST), Japan
+#     All rights reserved.
+#
+# $Id$
+#
+
+import os
+import shutil
+import glob
+import makewxs
+
+data = [
+    ("Source/jar",                                                      "*.jar"),
+    ("Source/examples/bat",                                             "*.bat"),
+    ("Source/examples/bin",                                             "*.vbs"),
+    ("Source/examples/RTMExamples/Composite",                           "*.class *.java *.conf"),
+    ("Source/examples/RTMExamples/ConfigSample",                        "*.class *.java *.conf"),
+    ("Source/examples/RTMExamples/GUIIn",                               "*.class *.java *.conf"),
+    ("Source/examples/RTMExamples/GUIIn/control",                       "*.class *.java"),
+    ("Source/examples/RTMExamples/GUIIn/model",                         "*.class *.java"),
+    ("Source/examples/RTMExamples/GUIIn/view",                          "*.class *.java"),
+    ("Source/examples/RTMExamples/SeqIO",                               "*.class *.java *.conf"),
+    ("Source/examples/RTMExamples/SeqIO/view",                          "*.class *.java"),
+    ("Source/examples/RTMExamples/SimpleIO",                            "*.class *.java *.conf"),
+    ("Source/examples/RTMExamples/SimpleService",                       "*.class *.java *.conf *.idl"),
+    ("Source/examples/RTMExamples/SinCosOut",                           "*.class *.java *.conf"),
+    ("Source/JavaDoc",                                                  "*.html *.css package-list"),
+    ("Source/JavaDoc/_SDOPackage",                                      "*.html"),
+    ("Source/JavaDoc/_SDOPackage/class-use",                            "*.html"),
+    ("Source/JavaDoc/index-files",                                      "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm",                                   "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/class-use",                         "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC",                               "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/buffer",                        "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/buffer/class-use",              "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/class-use",                     "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/executionContext",              "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/executionContext/class-use",    "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/log",                           "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/log/class-use",                 "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/port",                          "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/port/class-use",                "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/port/publisher",                "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/port/publisher/class-use",      "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/SDOPackage",                    "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/SDOPackage/class-use",          "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/util",                          "*.html"),
+    ("Source/JavaDoc/jp/go/aist/rtm/RTC/util/class-use",                "*.html"),
+    ("Source/JavaDoc/OpenRTM",                                          "*.html"),
+    ("Source/JavaDoc/OpenRTM/class-use",                                "*.html"),
+    ("Source/JavaDoc/resources",                                        "*.gif"),
+    ("Source/JavaDoc/RTC",                                              "*.html"),
+    ("Source/JavaDoc/RTC/class-use",                                    "*.html"),
+    ("Source/JavaDoc/RTM",                                              "*.html"),
+    ("Source/JavaDoc/RTM/class-use",                                    "*.html"),
+    ("Source/JavaDoc/RTMExamples/Composite",                            "*.html"),
+    ("Source/JavaDoc/RTMExamples/Composite/class-use",                  "*.html"),
+    ("Source/JavaDoc/RTMExamples/ConfigSample",                         "*.html"),
+    ("Source/JavaDoc/RTMExamples/ConfigSample/class-use",               "*.html"),
+    ("Source/JavaDoc/RTMExamples/GUIIn",                                "*.html"),
+    ("Source/JavaDoc/RTMExamples/GUIIn/class-use",                      "*.html"),
+    ("Source/JavaDoc/RTMExamples/GUIIn/control",                        "*.html"),
+    ("Source/JavaDoc/RTMExamples/GUIIn/control/class-use",              "*.html"),
+    ("Source/JavaDoc/RTMExamples/GUIIn/model",                          "*.html"),
+    ("Source/JavaDoc/RTMExamples/GUIIn/model/class-use",                "*.html"),
+    ("Source/JavaDoc/RTMExamples/GUIIn/view",                           "*.html"),
+    ("Source/JavaDoc/RTMExamples/GUIIn/view/class-use",                 "*.html"),
+    ("Source/JavaDoc/RTMExamples/SeqIO",                                "*.html"),
+    ("Source/JavaDoc/RTMExamples/SeqIO/class-use",                      "*.html"),
+    ("Source/JavaDoc/RTMExamples/SeqIO/view",                           "*.html"),
+    ("Source/JavaDoc/RTMExamples/SeqIO/view/class-use",                 "*.html"),
+    ("Source/JavaDoc/RTMExamples/SimpleIO",                             "*.html"),
+    ("Source/JavaDoc/RTMExamples/SimpleIO/class-use",                   "*.html"),
+    ("Source/JavaDoc/RTMExamples/SimpleService",                        "*.html"),
+    ("Source/JavaDoc/RTMExamples/SimpleService/class-use",              "*.html"),
+    ("Source/JavaDoc/RTMExamples/SinCosOut",                            "*.html"),
+    ("Source/JavaDoc/RTMExamples/SinCosOut/class-use",                  "*.html"),
+    ("Source/JavaDocEn",                                                "*.html *.css package-list"),
+    ("Source/JavaDocEn/_SDOPackage",                                    "*.html"),
+    ("Source/JavaDocEn/_SDOPackage/class-use",                          "*.html"),
+    ("Source/JavaDocEn/index-files",                                    "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm",                                 "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/class-use",                       "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC",                             "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/buffer",                      "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/buffer/class-use",            "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/class-use",                   "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/executionContext",            "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/executionContext/class-use",  "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/log",                         "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/log/class-use",               "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/port",                        "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/port/class-use",              "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/port/publisher",              "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/port/publisher/class-use",    "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/SDOPackage",                  "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/SDOPackage/class-use",        "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/util",                        "*.html"),
+    ("Source/JavaDocEn/jp/go/aist/rtm/RTC/util/class-use",              "*.html"),
+    ("Source/JavaDocEn/OpenRTM",                                        "*.html"),
+    ("Source/JavaDocEn/OpenRTM/class-use",                              "*.html"),
+    ("Source/JavaDocEn/resources",                                      "*.gif"),
+    ("Source/JavaDocEn/RTC",                                            "*.html"),
+    ("Source/JavaDocEn/RTC/class-use",                                  "*.html"),
+    ("Source/JavaDocEn/RTM",                                            "*.html"),
+    ("Source/JavaDocEn/RTM/class-use",                                  "*.html"),
+    ("Source/JavaDocEn/RTMExamples/Composite",                          "*.html"),
+    ("Source/JavaDocEn/RTMExamples/Composite/class-use",                "*.html"),
+    ("Source/JavaDocEn/RTMExamples/ConfigSample",                       "*.html"),
+    ("Source/JavaDocEn/RTMExamples/ConfigSample/class-use",             "*.html"),
+    ("Source/JavaDocEn/RTMExamples/GUIIn",                              "*.html"),
+    ("Source/JavaDocEn/RTMExamples/GUIIn/class-use",                    "*.html"),
+    ("Source/JavaDocEn/RTMExamples/GUIIn/control",                      "*.html"),
+    ("Source/JavaDocEn/RTMExamples/GUIIn/control/class-use",            "*.html"),
+    ("Source/JavaDocEn/RTMExamples/GUIIn/model",                        "*.html"),
+    ("Source/JavaDocEn/RTMExamples/GUIIn/model/class-use",              "*.html"),
+    ("Source/JavaDocEn/RTMExamples/GUIIn/view",                         "*.html"),
+    ("Source/JavaDocEn/RTMExamples/GUIIn/view/class-use",               "*.html"),
+    ("Source/JavaDocEn/RTMExamples/SeqIO",                              "*.html"),
+    ("Source/JavaDocEn/RTMExamples/SeqIO/class-use",                    "*.html"),
+    ("Source/JavaDocEn/RTMExamples/SeqIO/view",                         "*.html"),
+    ("Source/JavaDocEn/RTMExamples/SeqIO/view/class-use",               "*.html"),
+    ("Source/JavaDocEn/RTMExamples/SimpleIO",                           "*.html"),
+    ("Source/JavaDocEn/RTMExamples/SimpleIO/class-use",                 "*.html"),
+    ("Source/JavaDocEn/RTMExamples/SimpleService",                      "*.html"),
+    ("Source/JavaDocEn/RTMExamples/SimpleService/class-use",            "*.html"),
+    ("Source/JavaDocEn/RTMExamples/SinCosOut",                          "*.html"),
+    ("Source/JavaDocEn/RTMExamples/SinCosOut/class-use",                "*.html")
+]
+
+## Resource path
+##
+base_dir = os.getenv("OPENRTM_JAVA")
+if base_dir == None:
+    base_dir = "C:\\distribution\\OpenRTM-aist-Java-1.0.0\\"
+else:
+    base_dir = base_dir.replace("\"", "")
+    base_dir += "\\"
+
+
+## make temporary files
+##
+temp_dir = base_dir + "Source\\examples\\bin"
+bin_list = glob.glob(base_dir + "Source\\examples\\*.vbs")
+bin_cnt = len(bin_list)
+if os.path.exists(temp_dir) :
+    shutil.rmtree(temp_dir)
+os.mkdir(temp_dir)
+for i in range(bin_cnt):
+    shutil.copy2(bin_list[i], temp_dir)
+
+## Shortcut *.bat choice
+temp_dir = base_dir + "Source\\examples\\bat"
+bat_dir = base_dir + "Source\\examples\\"
+bat_list = ["Composite.bat", "ConfigSample.bat", "ConsoleIn.bat", "ConsoleOut.bat", 
+            "GUIIn.bat", "MyServiceConsumer.bat", "MyServiceProvider.bat", 
+            "SeqIn.bat", "SeqOut.bat", "SinCosOut.bat"]
+bin_cnt = len(bat_list)
+bin_list = []
+for i in range(bin_cnt):
+    bin_list.append(bat_dir + bat_list[i])
+if os.path.exists(temp_dir) :
+    shutil.rmtree(temp_dir)
+os.mkdir(temp_dir)
+for i in range(bin_cnt):
+    shutil.copy2(bin_list[i], temp_dir)
+
+
+def path_to_dir_id(path, prefix):
+    # path = "bin/x86_win32" prefix = "omni"
+    # output = "omni_bin_x86_win32"
+    # "." -> "_"
+    output = prefix + "_" + "_".join(path.split("/"))
+    return output.replace(".", "_")
+
+def path_to_comp_id(path, prefix):
+    # path = "bin/x86_win32" prefix = "omni"
+    # output = "OmniBinX86_win32"
+    # "." -> "_"
+    output = prefix.capitalize()
+    for c in path.split("/"):
+        output += c.capitalize()
+    return output.replace(".", "_")
+
+
+## make yaml file
+##
+for (path, files) in data:
+    # wxs component name
+    comp_name = path_to_comp_id(path, "rtm")
+    comp_name = comp_name.replace("-", "")
+    # wxs directory name
+    dir_name = path_to_dir_id(path, "rtm")
+
+    path = path.replace("/", "\\")
+
+    # full path to target directory
+    full_path = base_dir + "\\" + path
+
+    flist = []
+    for f in files.split(" "):
+        flist += glob.glob(full_path + "\\" + f)
+
+    curr_path = base_dir + path
+    if curr_path.rfind("\\") == (len(curr_path) -1):
+        curr_path = curr_path[0:len(curr_path)-1]
+
+    cmd = ["flist",
+           "-c", comp_name,
+           "-o", dir_name + ".yaml",
+           "-p", curr_path]
+    cmd += flist
+    makewxs.main(cmd)
+
+
+## make wxs file
+##
+cmd = ["wxs",
+       "-o", "OpenRTM-aist-Java.wxs",
+       "-i", "OpenRTM-aist-Java.wxs.in"]
+cmd += glob.glob("*.yaml")
+makewxs.main(cmd)
+
---
+++ jp.go.aist.rtm.RTC/installer/win32/build.cmd
@@ -0,0 +1,116 @@
+
+@rem ------------------------------------------------------------
+@rem Variable Settings
+@rem   usually only %TARGET% might be changed
+@rem ------------------------------------------------------------
+@set PATH=%WIX%\bin;%PATH%
+@set VERSION=1.0.0
+@set TARGET=OpenRTM-aist-Java
+@set TARGET_WXS=%TARGET%.wxs
+@set TARGET_WIXOBJ=%TARGET%.wixobj
+@set TARGET_FULL=%TARGET%-%VERSION%
+
+@rem ------------------------------------------------------------
+@rem WixUI Customization Settings
+@rem   usually only %WIXUI_RTM% might be changed
+@rem ------------------------------------------------------------
+@set WIXUI_RTM=WixUI_Mondo_rtm
+@set WIXUI_RTM_WXS=%WIXUI_RTM%.wxs
+@set WIXUI_RTM_WIXOBJ=%WIXUI_RTM%.wixobj
+
+@rem ------------------------------------------------------------
+@rem default distribution package folder
+@rem ------------------------------------------------------------
+@set DISTRIBUTION=C:\distribution
+@set OPENRTM_JAVA=%DISTRIBUTION%\OpenRTM-aist-Java-1.0.0
+
+@rem ------------------------------------------------------------
+@rem Supported languages
+@rem   supported languages have to be specified
+@rem ------------------------------------------------------------
+set LANGUAGES=(ja-jp de-de es-es fr-fr hu-hu it-it ko-kr zh-tw)
+
+echo off
+@rem ------------------------------------------------------------
+@rem Checking WiX
+@rem ------------------------------------------------------------
+if "x%WIX%" == "x" (
+   echo "Windows Installer XML (WiX) is not installed"
+   echo "Please download WiX 3.5 or later from http://wix.sourceforge.net/"
+   goto END
+)
+
+@rem ------------------------------------------------------------
+@rem Import Language-Country, Language codes, Codepages
+@rem from langs.txt
+@rem http://www.tramontana.co.hu/wix/lesson2.php#2.4
+@rem ------------------------------------------------------------
+for /F "tokens=1,2,3,4 delims=, " %%i in (langs.txt) do (
+    set LC[%%j]=%%j
+    set LANG[%%j]=%%k
+    set CODE[%%j]=%%l
+)
+
+@rem ============================================================
+@rem Make OpenRTM-aist-Java file list
+@rem ============================================================
+python OpenRTMjavawxs.py
+
+@rem ============================================================
+@rem compile wxs file and link msi
+@rem ============================================================
+candle.exe %TARGET_WXS% %WIXUI_RTM_WXS% -dlanguage=1033 -dcodepage=1252
+light.exe -ext WixUIExtension -loc WixUI_en-us.wxl ^
+      	       -out %TARGET_FULL%.msi %TARGET_WIXOBJ% %WIXUI_RTM_WIXOBJ%
+
+set IDS=1033
+setlocal ENABLEDELAYEDEXPANSION
+
+for %%i in %LANGUAGES% do (
+
+    @rem ------------------------------------------------------------
+    @rem language ID list
+    @rem
+    set IDS=!IDS!,!LANG[%%i]!
+
+    @rem ------------------------------------------------------------
+    @rem compile wxs file and link msi
+    @rem
+    candle.exe %TARGET_WXS% %WIXUI_RTM_WXS% -dlanguage=!LANG[%%i]! -dcodepage=!CODE[%%i]!
+
+    if exist WixUI_!LC[%%i]!.wxl (
+       light.exe -ext WixUIExtension -loc WixUI_!LC[%%i]!.wxl ^
+            -out %TARGET_FULL%_!LC[%%i]!.msi %TARGET_WIXOBJ% %WIXUI_RTM_WIXOBJ%
+    )
+    if not exist WixUI_!LC[%%i]!.wxl (
+       light.exe -ext WixUIExtension -cultures:!LC[%%i]! ^
+            -out %TARGET_FULL%_!LC[%%i]!.msi %TARGET_WIXOBJ% %WIXUI_RTM_WIXOBJ%
+    )
+    @rem ------------------------------------------------------------
+    @rem creating transformation files
+    @rem
+    torch.exe -p -t language %TARGET_FULL%.msi %TARGET_FULL%_!LC[%%i]!.msi ^
+    	      -out !LC[%%i]!.mst
+
+    @rem ------------------------------------------------------------
+    @rem embed transformation files
+    @rem
+    cscript wisubstg.vbs %TARGET_FULL%.msi !LC[%%i]!.mst !LANG[%%i]!
+
+)
+
+@rem ------------------------------------------------------------
+@rem here mst embedded msi can be selected languages by 
+@rem > msiexec /i SampleMulti.msi TRANSFORMS=":fr-fr.mst"
+@rem
+
+@rem ------------------------------------------------------------
+@rem Update the summary information stream to list all
+@rem supported languages of this package
+@rem ------------------------------------------------------------
+cscript WiLangId.vbs %TARGET_FULL%.msi Package %IDS%
+
+
+:END
+del *.yaml
+
