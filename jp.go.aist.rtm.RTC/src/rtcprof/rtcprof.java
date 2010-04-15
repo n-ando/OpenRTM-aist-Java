0521951362be7dcb1e9ee49fa0416e350772348b
fsi-katami <openrtm@openrtm.org>
2010-04-15 02:48:49 +0000

    Modified rtcprof. refs 1361
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0_0@354 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/rtcprof/rtcprof.java
+++ jp.go.aist.rtm.RTC/src/rtcprof/rtcprof.java
@@ -3,13 +3,22 @@ package rtcprof;
 import java.util.Vector;
 import java.lang.reflect.Field;
 import java.io.File;
+import java.net.URI;
+import java.net.URLClassLoader;
+import java.net.URL;
+
 
 
 public class rtcprof {
 
+    /**
+     * 
+     */
     private static Class getClassFromName(String name){
-        String separator =  "/";
+System.err.println("getClassFromName");
+        String separator =  System.getProperty("file.separator");
         Class target = null;
+
         try {
             target = Class.forName(name);
         } catch (java.lang.NoClassDefFoundError e) {
@@ -31,6 +40,78 @@ public class rtcprof {
     }
 
     /**
+     * 
+     */
+    private static Class getClassFromName(URLClassLoader url, 
+                                           String name){
+        String separator =  System.getProperty("file.separator");
+        Class target = null;
+
+        try {
+            target = url.loadClass(name);
+        } catch (java.lang.NoClassDefFoundError e) {
+            String messagetString = e.getMessage();
+            String key = "wrong name: ";
+            int index = messagetString.indexOf(key);
+            String packageName 
+                = messagetString.substring(index+key.length(),
+                                               messagetString.length()-1);
+
+            URL[] urls = url.getURLs();
+            java.util.ArrayList al 
+                    = new java.util.ArrayList(java.util.Arrays.asList(urls));
+            for(int ic=0;ic<urls.length;++ic){
+                String stringPath = new String();
+                String stringUrl = urls[ic].getPath();
+                int pointer = packageName.lastIndexOf(name);
+                String stringPackageName = packageName.substring(0, pointer);
+                if(stringUrl.endsWith(stringPackageName)){
+                    int point = stringUrl.lastIndexOf(stringPackageName);
+                    stringPath = stringUrl.substring(0, point);
+                    File path = new File(stringPath);
+                    try{
+                        URI uri = path.toURI();
+                        al.add(uri.toURL());
+                    }
+                    catch(java.net.MalformedURLException ex){
+                        System.err.println(
+                       "java.net.MalformedURLException: toURL() threw Exception."+ex);
+                    }
+                }
+            }
+            URL[] addUrls = (URL[])al.toArray(new URL[]{});
+            url = url.newInstance(addUrls, url);
+
+            packageName = packageName.replace(separator,".");
+            packageName = packageName.trim();
+
+
+            target = rtcprof.getClassFromName(url,packageName);
+        } catch (Exception e) {
+            System.err.println("    Caught.Exception:"+e);
+        }
+        return target;
+    }
+
+    /**
+     *
+     */
+    private static URLClassLoader createURLClassLoader(String parent){
+        File path = new File(parent);
+        URL[] urls = new URL[1];
+        try{
+            URI uri = path.toURI();
+            urls[0] = uri.toURL();
+        }
+        catch(java.net.MalformedURLException ex){
+            System.err.println(
+                "java.net.MalformedURLException: toURL() threw Exception."+ex);
+            return null;
+        }
+        URLClassLoader url = new URLClassLoader(urls);
+        return url;
+    }
+    /**
      * フルパスで指定されたコンポーネント（jarファイルとclassファイル）の
      * スペックを読み込み表示する。
      *
@@ -51,31 +132,36 @@ public class rtcprof {
         Class target = null;
         File file = new File(args[0]);
         if(file.isAbsolute()) {
-            args[0] = file.getName();
-            String extensions[] = {".class", ".jar"};
-            for(int ic=0;ic<extensions.length;++ic){
-                if(args[0].endsWith(extensions[ic])){
-                    int point = args[0].lastIndexOf(extensions[ic]);
-                    args[0] =  args[0].substring(0, point);
-                    if(extensions[ic].equals(".jar")){
-                        args[0] =  args[0]+"."+args[0];
+            URLClassLoader url = createURLClassLoader(file.getParent());
+            if(url!=null){
+                String name = file.getName();
+                String extensions[] = {".class", ".jar"};
+                for(int ic=0;ic<extensions.length;++ic){
+                    if(name.endsWith(extensions[ic])){
+                        int point = name.lastIndexOf(extensions[ic]);
+                        name = name.substring(0, point);
+                        if(extensions[ic].equals(".jar")){
+                            name =  name+"."+name;
+                        }
+                        break;
                     }
-                    break;
                 }
+                target = getClassFromName(url,name);
             }
         }
         else{
             args[0] = args[0].replace(separator,".");
             args[0] = args[0].replace("..",".");
+            target = rtcprof.getClassFromName(args[0]);
         }
 
-        target = rtcprof.getClassFromName(args[0]);
 
         if(target != null){
             try {
                 Field field = target.getField("component_conf");
                 String[] data = (String[])field.get(null);
                 for(int ic=0;ic<data.length/2;++ic){
+                    System.err.println(data[ic*2] +": "+data[ic*2+1]);
                     System.out.println(data[ic*2] +": "+data[ic*2+1]);
                 }
             }
