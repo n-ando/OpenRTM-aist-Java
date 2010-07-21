ed0e488e2ca0ed6402ad46d62db89e3f91b2efa4
fsi-katami <openrtm@openrtm.org>
2010-07-21 05:41:59 +0000

    Updated the comment.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0@448 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/buffer/ReturnCode.java
+++ jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/buffer/ReturnCode.java
@@ -1,5 +1,33 @@
 package jp.go.aist.rtm.RTC.buffer;
 
+
+    /**
+     * {@.ja BufferStatus リターンコード}
+     * {@.en iBufferStatus return codes}
+     *
+     * <p>
+     * {@.ja バッファ関連のクラスで共通のリターンコード
+     * <ul>
+     * <li> BUFFER_OK:            正常終了
+     * <li> BUFFER_ERROR:         バッファエラー
+     * <li> BUFFER_FULL:          バッファフル
+     * <li> BUFFER_EMPTY:         バッファエンプティ
+     * <li> NOT_SUPPORTED:        未サポート機能
+     * <li> TIMEOUT:              タイムアウト
+     * <li> PRECONDITION_NOT_MET: 事前条件を満たしていない
+     * </ul>}
+     * {@.en Common return codes for buffer classes.
+     * <ul>
+     * <li> BUFFER_OK:            Normal return
+     * <li> BUFFER_ERROR:         Buffer error
+     * <li> BUFFER_FULL:          Buffer full
+     * <li> BUFFER_EMPTY:         Buffer empty
+     * <li> NOT_SUPPORTED:        Not supported function
+     * <li> TIMEOUT:              Timeout
+     * <li> PRECONDITION_NOT_MET: Precodition not met
+     * </ul>}
+     *
+     */
 public enum ReturnCode {
     BUFFER_OK,
     BUFFER_ERROR,
