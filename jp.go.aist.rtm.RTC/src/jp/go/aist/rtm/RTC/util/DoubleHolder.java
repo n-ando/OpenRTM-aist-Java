04a87ee8fc7e8f4bc5643df5c082b679994988c4
fsi-katami <openrtm@openrtm.org>
2010-10-29 14:01:37 +0000

    Updated the comment.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0@467 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/util/DoubleHolder.java
+++ jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/util/DoubleHolder.java
@@ -3,74 +3,111 @@ package jp.go.aist.rtm.RTC.util;
 import java.io.Serializable;
 
 /**
- * <p>double型データ格納用クラスです。</p>
+ * {@.ja double型データ格納用クラス}
+ * {@.en Class for double type data storage}
  */
 public class DoubleHolder implements ValueHolder, Serializable {
 
     private static final long serialVersionUID = 8090384116386302112L;
     
     /**
-     * <p>double型データ設定値</p>
+     * {@.ja double型データ設定値}
+     * {@.en dpuble type data setting value}
      */
     public Double value = null;
 
     /**
-     * <p>デフォルトコンストラクタです。</p>
+     * {@.ja デフォルトコンストラクタ}
+     * {@.en Default constructor}
      */
     public DoubleHolder() {
     }
 
     /**
-     * <p>コンストラクタです。</p>
-     * 
-     * @param initialValue 初期値
+     * {@.ja コンストラクタ}
+     * {@.en Constructor}
+     *
+     * @param initialValue
+     *   {@.ja 初期値}
+     *   {@.en Initial value}
      */
     public DoubleHolder(double initialValue) {
         value = new Double(initialValue);
     }
 
     /**
-     * <p>コンストラクタです。</p>
-     * 
-     * @param initialValue 初期値
+     * {@.ja コンストラクタ}
+     * {@.en Constructor}
+     *
+     * @param initialValue
+     *   {@.ja 初期値}
+     *   {@.en Initial value}
      */
     public DoubleHolder(Double initialValue) {
         value = initialValue;
     }
 
     /**
-     * <p>文字列からdouble型に変換して設定します。</p>
+     * {@.ja 文字列からdouble型に変換して設定する}
+     * {@.en Converts from String into double type and sets it.}
      * 
-     * @param def_val 設定値文字列表現
-     * @exception Exception 渡された文字列が構文解析可能な double 値を含まない。
+     * @param def_val
+     *   {@.ja 設定値文字列表現}
+     *   {@.en String}
+     * 
+     * @exception Exception 
+     *   {@.ja 渡された文字列が構文解析可能な double 値を含まない。}
+     *   {@.en The character string of the argument is not good at parsing.}
      */
     public void stringFrom(String def_val) throws Exception {
         value = new Double(def_val);
     }
 
     /**
-     * <p>設定値を取得します。</p>
-     * 
-     * @return 設定値
+     * {@.ja 設定値を取得する}
+     * {@.en Gets a set value.}
+     *
+　   * @return 
+     *   {@.ja 設定値}
+     *   {@.en Set value}
      */
     public double getValue() {
         return value.doubleValue();
     }
 
     /**
-     * <p>設定値を文字列に変換して取得します。</p>
-     * 
-     * @return 設定値文字列表現
+     * {@.ja 設定値を文字列に変換して取得する}
+     * {@.en Converts a set value into the character string and gets it. }
+     *
+　   * @return 
+     *   {@.ja 設定値文字列表現}
+     *   {@.en Converted character string}
      */
     public String toString() {
         return String.valueOf(value);
     }
+    /**
+     * {@.ja InputStreamのデータを読み込む}
+     * {@.en Reads data from InputStream.}
+     *
+     * @param i
+     *   {@.ja InputStream}
+     *   {@.en InputStream}
+     */
     public void _read (org.omg.CORBA.portable.InputStream i)
     {
         double data = i.read_double();
         value = new Double(data);
     }
 
+    /**
+     * {@.ja OutputStreamへデータを書き込む}
+     * {@.en Writes data in OutputStream}
+     *
+     * @param o
+     *   {@.ja OutputStream}
+     *   {@.en OutputStream}
+     */
     public void _write (org.omg.CORBA.portable.OutputStream o)
     {
         o.write_double(value.doubleValue());
