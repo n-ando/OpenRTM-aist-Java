4bcfef3ad8121f1ab1764189e99ac19bdf4c833a
fsi-katami <openrtm@openrtm.org>
2010-10-29 05:50:59 +0000

    Updated the comment.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0@462 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/util/OnUpdateCallbackFunc.java
+++ jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/util/OnUpdateCallbackFunc.java
@@ -1,14 +1,18 @@
 package jp.go.aist.rtm.RTC.util;
 
 /**
- * <p>Callbackクラスのインターフェースです。</p>
+ * {@.ja Callbackクラスのインターフェース}
+ * {@.en Interface of Callback class}
  */
 public interface OnUpdateCallbackFunc {
     
     /**
-     * <p>対象のオブジェクトに対して操作を行います。</p>
+     * {@.ja 対象のオブジェクトに対して操作を行う}
+     * {@.en Operates object}
      *
-     * @param config_set コンフィグレーション
+     * @param config_set 
+     *   {@.ja コンフィグレーション}
+     *   {@.en Configuration}
      */
 
     public void operator(String config_set);
