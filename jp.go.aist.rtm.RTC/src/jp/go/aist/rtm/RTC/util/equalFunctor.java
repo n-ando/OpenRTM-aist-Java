59e23805b760610fb881251265de307820a1bcc1
fsi-katami <openrtm@openrtm.org>
2010-11-02 06:33:27 +0000

    Updated the comment.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0@469 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/util/equalFunctor.java
+++ jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/util/equalFunctor.java
@@ -1,15 +1,21 @@
 package jp.go.aist.rtm.RTC.util;
 
 /**
- * <p>条件合致判定に使用する汎用的なファンクタインタフェースです。</p>
+ * {@.ja 条件合致判定に使用する汎用的なファンクタインタフェース}
+ * {@.en General interface used to judge condition agreement}
  */
 public interface equalFunctor {
     
     /**
-     * <p>条件に合致するかどうか判定します。</p>
+     * {@.ja 条件に合致するかどうか判定する}
+     * {@.en Judges whether to agree contingent on the object.}
      * 
-     * @param object 判定対象のオブジェクト
-     * @return 合致する場合はtrueを、さもなくばfalseを返します。
+     * @param object 
+     *   {@.ja 判定対象のオブジェクト}
+     *   {@.en Object to be judged}
+     * @return 
+     *   {@.ja 合致する場合はtrueを、さもなくばfalseを返す。}
+     *   {@.en The agreement is true.}
      */
     public boolean equalof(final java.lang.Object object);
 }
