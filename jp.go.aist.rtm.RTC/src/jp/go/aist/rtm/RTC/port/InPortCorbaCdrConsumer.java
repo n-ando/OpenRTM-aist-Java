0b26b91fd5054d52d974f50b3460275ee57a2f5a
fsi-katami <openrtm@openrtm.org>
2010-12-02 08:58:31 +0000

    Updated the comment.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/RELENG_1_0@486 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/port/InPortCorbaCdrConsumer.java
+++ jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/port/InPortCorbaCdrConsumer.java
@@ -18,16 +18,25 @@ import _SDOPackage.NVListHolder;
 import com.sun.corba.se.impl.encoding.EncapsOutputStream; 
 
 /**
- * <p> InPortCorbaCdrConsumer </p>
- * <p> InPortCorbaCdrConsumer class </p>
- * <p> This is an implementation class of the input port Consumer  </p>
- * <p> that uses CORBA for means of communication. </p>
+ * {@.ja InPortCorbaCdrConsumer クラス}
+ * {@.en InPortCorbaCdrConsumer class}
+ * <p>
+ * {@.ja データ転送に CORBA の OpenRTM::InPortCdr インターフェースを利用し
+ * た、push 型データフロー型を実現する InPort コンシューマクラス。}
+ * {@.en This is an implementation class of the input port Consumer  </p>
+ * that uses CORBA for means of communication. </p>
+ * The InPort consumer class which uses the OpenRTM::InPortCdr
+ * interface in CORBA for data transfer and realizes a push-type
+ * dataflow.}
  */
 
 public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > implements InPortConsumer, ObjectCreator<InPortConsumer>, ObjectDestructor {
     /**
-     * <p> Constructor </p>
-     * <p> buffer The buffer object that is attached to this Consumer </p>
+     * {@.ja コンストラクタ}
+     * {@.en Constructor}
+     * <p>
+     * {@.en buffer 当該コンシューマに割り当てるバッファオブジェクト}
+     * {@.en buffer The buffer object that is attached to this Consumer}
      *
      */
     public InPortCorbaCdrConsumer() {
@@ -39,10 +48,24 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
         m_orb = ORBUtil.getOrb();
     }
     /**
-     * <p> Initializing configuration </p>
-     * <p> This operation would be called to configure this consumer </p>
-     * <p> in initialization. </p>
-     *
+     * {@.ja 設定初期化}
+     * {@.en Initializing configuration}
+     * <p>
+     * {@.ja InPortConsumerの各種設定を行う。実装クラスでは、与えられた
+     * Propertiesから必要な情報を取得して各種設定を行う。この init() 関
+     * 数は、InPortProvider生成直後および、接続時にそれぞれ呼ばれる可
+     * 能性がある。したがって、この関数は複数回呼ばれることを想定して記
+     * 述されるべきである。}
+     * {@.en This operation would be called to configure in initialization.
+     * In the concrete class, configuration should be performed
+     * getting appropriate information from the given Properties data.
+     * This function might be called right after instantiation and
+     * connection sequence respectivly.  Therefore, this function
+     * should be implemented assuming multiple call.}
+     * 
+     * @param prop 
+     *   {@.ja 設定情報}
+     *   {@.en Configuration information}
      */
     public void init(Properties prop) {
         m_properties = prop;
@@ -52,6 +75,36 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
      * <p> Send data to the destination port </p>
      * <p> Pure virtual function to send data to the destination port. </p>
      *
+     */
+    /**
+     * {@.ja 接続先へのデータ送信}
+     * {@.en Send data to the destination port}
+     * <p>
+     * {@.ja 接続先のポートへデータを送信するための純粋仮想関数。
+     * 
+     * この関数は、以下のリターンコードを返す。
+     * <ul>
+     * <li>- PORT_OK:     正常終了。
+     * <li>- PORT_ERROR:  データ送信の過程で何らかのエラーが発生した。
+     * <li>- SEND_FULL:   データを送信したが、相手側バッファがフルだった。
+     * <li>- SEND_TIMEOUT:データを送信したが、相手側バッファがタイムアウトした。
+     * <li>- UNKNOWN_ERROR:原因不明のエラー</ul>}
+     * {@.en Pure virtual function to send data to the destination port.
+     *
+     * This function might the following return codes
+     * <ul>
+     * <li>- PORT_OK:       Normal return
+     * <li>- PORT_ERROR:    Error occurred in data transfer process
+     * <li>- SEND_FULL:     Buffer full although OutPort tried to send data
+     * <li>- SEND_TIMEOUT:  Timeout although OutPort tried to send data
+     * <li>- UNKNOWN_ERROR: Unknown error</ul>}
+     *
+     * @param data 
+     *   {@.ja 送信するデータ}
+     *   {@.en Data sent by this operation.}
+     * @return 
+     *   {@.ja リターンコード}
+     *   {@.en ReturnCode}
      */
     public ReturnCode put(final OutputStream data) {
         rtcout.println(Logbuf.PARANOID, "put");
@@ -73,26 +126,41 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
         }
     }
     /**
-     * <p> Publish InterfaceProfile information </p>
+     * {@.ja InterfaceProfile情報を公開する}
+     * {@.en Publish InterfaceProfile information}
+     * <p>
+     * {@.ja InterfaceProfile情報を公開する。
+     * 引数で指定するプロパティ情報内の NameValue オブジェクトの
+     * dataport.interface_type 値を調べ、当該ポートに設定されている
+     * インターフェースタイプと一致する場合のみ情報を取得する。}
+     * {@.en Publish interfaceProfile information.
+     * Check the dataport.interface_type value of the NameValue object 
+     * specified by an argument in property information and get information
+     * only when the interface type of the specified port is matched.}
      *
-     * <p> Publish interfaceProfile information. </p>
-     * <p> Check the dataport.interface_type value of the NameValue object </p>
-     * <p> specified by an argument in property information and get information </p>
-     * <p> only when the interface type of the specified port is matched. </p>
-     *
-     * @param properties Properties to get InterfaceProfile information
-     *
+     * @param properties 
+     *   {@.ja InterfaceProfile情報を受け取るプロパティ}
+     *   {@.en Properties to get InterfaceProfile information}
      */
     public void publishInterfaceProfile(NVListHolder properties) {
         return;
     }
     /**
-     * <p> Subscribe to the data sending notification </p>
-     * <p> Subscribe to the data sending notification based on specified  </p>
-     * <p> property information. </p>
-     * @param properties Information for subscription
-     * @return Subscription result (Successful:true, Failed:false)
+     * {@.ja データ送信通知への登録}
+     * {@.en Subscribe to the data sending notification}
+     * <p>
+     * {@.ja 指定されたプロパティに基づいて、データ送出通知の受け取り
+     * に登録する。}
+     * {@.en Subscribe to the data sending notification based on specified 
+     * property information.}
      *
+     * @param properties 
+     *   {@.ja 登録情報}
+     *   {@.en Information for subscription}
+     *
+     * @return 
+     *   {@.ja 登録処理結果(登録成功:true、登録失敗:false)}
+     *   {@.en Subscription result (Successful:true, Failed:false)}
      */
     public boolean subscribeInterface(final NVListHolder properties) {
         rtcout.println(Logbuf.TRACE, "subscribeInterface()");
@@ -118,10 +186,15 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
     }
     
     /**
-     * <p> Unsubscribe the data send notification </p>
-     * <p> Unsubscribe the data send notification. </p>
-     * @param properties Information for unsubscription
+     * {@.ja データ送信通知からの登録解除}
+     * {@.en Unsubscribe the data send notification}
+     * <p>
+     * {@.ja データ送出通知の受け取りから登録を解除する。}
+     * {@.en Unsubscribe the data send notification.}
      *
+     * @param properties 
+     *   {@.ja 登録解除情報}
+     *   {@.en Information for unsubscription}
      */
     public void unsubscribeInterface(final NVListHolder properties) {
         rtcout.println(Logbuf.TRACE, "unsubscribeInterface()");
@@ -134,10 +207,15 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
     }
 
     /**
-     * <p> Getting object reference fromn IOR string </p>
-     * @param properties Information for subscription
-     * @return true: succeeded, false: failed
+     * {@.ja IOR文字列からオブジェクト参照を取得する}
+     * {@.en Getting object reference fromn IOR string}
+     * @param properties 
+     *   {@.ja Information for subscription}
+     *   {@.en Information for subscription}
      *
+     * @return 
+     *   {@.ja true: 正常取得, false: 取得失敗}
+     *   {@.en true: succeeded, false: failed}
      */
     private boolean subscribeFromIor(final NVListHolder properties) {
         rtcout.println(Logbuf.TRACE, "subscribeFromIor()");
@@ -184,9 +262,14 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
     }
 
     /**
-     * <p> Getting object reference fromn Any directry </p>
-     * @param properties Information for subscription
-     * @return true: succeeded, false: failed
+     * {@.ja Anyから直接オブジェクト参照を取得する}
+     * {@.en Getting object reference fromn Any directry}
+     * @param properties 
+     *   {@.ja Information for subscription}
+     *   {@.en Information for subscription}
+     * @return 
+     *   {@.ja true: 正常取得, false: 取得失敗}
+     *   {@.en true: succeeded, false: failed}
      *
      */
     private boolean subscribeFromRef(final NVListHolder properties) {
@@ -221,10 +304,16 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
     }
 
     /**
-     * <p> ubsubscribing (IOR version) </p>
-     * @param properties Information for unsubscription
-     * @return true: succeeded, false: failed
+     * {@.ja 接続解除(IOR版)}
+     * {@.en ubsubscribing (IOR version)}
      *
+     * @param properties 
+     *   {@.ja Information for unsubscription}
+     *   {@.en Information for unsubscription}
+     *
+     * @return 
+     *   {@.ja true: 正常取得, false: 取得失敗}
+     *   {@.en true: succeeded, false: failed}
      */
     private boolean unsubscribeFromIor(final NVListHolder properties) {
         rtcout.println(Logbuf.TRACE, "unsubscribeFromIor()");
@@ -262,10 +351,15 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
     }
 
     /**
-     * <p> ubsubscribing (Object reference version) </p>
-     * @param properties Information for unsubscription
-     * @return true: succeeded, false: failed
+     * {@.ja 接続解除(Object reference版)}
+     * {@.en ubsubscribing (Object reference version)}
      *
+     * @param properties 
+     *   {@.ja Information for unsubscription}
+     *   {@.en Information for unsubscription}
+     * @return 
+     *   {@.ja true: 正常取得, false: 取得失敗}
+     *   {@.en true: succeeded, false: failed}
      */
     private boolean unsubscribeFromRef(final NVListHolder properties) {
         rtcout.println(Logbuf.TRACE, "unsubscribeFromRef()");
@@ -294,8 +388,15 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
         return true;
     }
     /**
-     * <p> convertReturn </p>
-     *
+     * {@.ja PortStatusをReturnCodeに変換する。}
+     * {@.en Converts PortStatus into ReturnCode.}
+     * 
+     * @param status
+     *   {@.ja PortStatus}
+     *   {@.en PortStatus}
+     * @return
+     *   {@.ja ReturnCode}
+     *   {@.en ReturnCode}
      */
     protected ReturnCode convertReturn(OpenRTM.PortStatus status) {
         switch (status.value()) {
@@ -315,26 +416,37 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
     }
     
     /**
-     * <p> creator_ </p>
+     * {@.ja InPortCorbaCdrConsumer を生成する}
+     * {@.en Creats InPortCorbaCdrConsumer}
      * 
-     * @return Object Created instances
+     * @return 
+     *   {@.ja 生成されたInPortConsumer}
+     *   {@.en Object Created instances}
+     *
      *
      */
     public InPortConsumer creator_() {
         return new InPortCorbaCdrConsumer();
     }
     /**
-     * <p> destructor_ </p>
+     * {@.ja Object を破棄する}
+     * {@.en Destructs Object}
      * 
-     * @param obj    The target instances for destruction
+     * @param obj
+     *   {@.ja 破棄するインタスタンス}
+     *   {@.en The target instances for destruction}
      *
      */
     public void destructor_(Object obj) {
         obj = null;
     }
     /**
-     * <p> InPortCorbaCdrConsumerInit </p>
-     *
+     * {@.ja モジュール初期化関数}
+     * {@.en Module initialization}
+     * <p>
+     * {@.ja InPortCorbaCdrConsumer のファクトリを登録する初期化関数。}
+     * {@.en This initialization function registers InPortCorbaCdrConsumer's 
+     * factory.}
      */
     public static void InPortCorbaCdrConsumerInit() {
         final InPortConsumerFactory<InPortConsumer,String> factory 
@@ -346,8 +458,12 @@ public class InPortCorbaCdrConsumer extends CorbaConsumer< OpenRTM.InPortCdr > i
     
     }
     /**
-     * <p> setConnecotor </p>
-     * @param connector
+     * {@.ja Connectorを設定する。}
+     * {@.en set Connector}
+     *
+     * @param connector 
+     *   {@.ja OutPortConnector}
+     *   {@.en OutPortConnector}
      */
     public void setConnector(OutPortConnector connector) {
         m_connector = connector;
