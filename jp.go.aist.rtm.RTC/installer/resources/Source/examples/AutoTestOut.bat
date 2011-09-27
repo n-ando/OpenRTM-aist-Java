48fb15b21e807d7c76678fd8a68d07ad496fad82
fsi-katami <openrtm@openrtm.org>
2011-09-27 06:38:25 +0000

    Fixed bat files
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/trunk@571 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/installer/resources/Source/examples/AutoTestOut.bat
+++ jp.go.aist.rtm.RTC/installer/resources/Source/examples/AutoTestOut.bat
@@ -1,3 +1,3 @@
-set CLASSPATH=.;%RTM_JAVA_ROOT%\jar\OpenRTM-aist-1.0.0.jar;%RTM_JAVA_ROOT%\jar\commons-cli-1.1.jar
+set CLASSPATH=.;%RTM_JAVA_ROOT%\jar\OpenRTM-aist-1.1.0.jar;%RTM_JAVA_ROOT%\jar\commons-cli-1.1.jar
 java RTMExamples.AutoTest.AutoTestOutComp -f RTMExamples\AutoTest\rtc.conf %*
 pause;
--- jp.go.aist.rtm.RTC/installer/resources/Source/examples/rtcprof_java.bat
+++ jp.go.aist.rtm.RTC/installer/resources/Source/examples/rtcprof_java.bat
@@ -10,6 +10,6 @@ goto exec
 set _cpath_=%~f1
 
 :exec
-set CLASSPATH=.;%RTM_JAVA_ROOT%\jar\OpenRTM-aist-1.0.0.jar;%RTM_JAVA_ROOT%\jar\commons-cli-1.1.jar;%RTM_JAVA_ROOT%\jar\rtcprof.jar;%_cpath_%
+set CLASSPATH=.;%RTM_JAVA_ROOT%\jar\OpenRTM-aist-1.1.0.jar;%RTM_JAVA_ROOT%\jar\commons-cli-1.1.jar;%RTM_JAVA_ROOT%\jar\rtcprof.jar;%_cpath_%
 java -cp "%CLASSPATH%;" rtcprof.rtcprof %1
 
