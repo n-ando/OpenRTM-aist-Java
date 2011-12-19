2fb1de6276e1fe510fa19c1f05a892e8beb462cb
fsi-katami <openrtm@openrtm.org>
2011-12-19 08:47:04 +0000

    Added the build for JacORB.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/branches/work@600 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- SourceSharing/buildRTC.sh
+++ SourceSharing/buildRTC.sh
@@ -2,21 +2,30 @@
 #
 #
 
-set DUMMY=$ANT_HOME
+DUMMY=$ANT_HOME
 export ANT_HOME=$ECLIPSE_HOME/plugins/org.apache.ant_1.6.5/
+
+ 
+
+if test $# -eq 0 ; then
+    var_for="buildAllLinux"
+else
+    var_for=$1
+fi
 
 #
 #
 #
 cd jp.go.aist.rtm.RTC
-ant buildAllLinux -lib $ECLIPSE_HOME/plugins/net.sf.ant4eclipse.plugin_0.5.0.rc1/lib/
+#ant buildAllLinux -lib $ECLIPSE_HOME/plugins/net.sf.ant4eclipse.plugin_0.5.0.rc1/lib/
+ant $var_for -lib $ECLIPSE_HOME/plugins/net.sf.ant4eclipse.plugin_0.5.0.rc1/lib/
 if [ $? -ne 0 ];
 then 
- exit 1
+    exit 1
 fi
 echo "--"
 cd ..
 
-set ANT_HOME=$DUMMY
+export ANT_HOME=$DUMMY
 
 
