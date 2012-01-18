461073ef7bf7ff6bd9fae5e58f00017a897bff93
fsi-katami <openrtm@openrtm.org>
2012-01-18 07:21:49 +0000

    [compat] Fixed the script for the build. refs #1990
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/trunk@647 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- buildRTC.bat
+++ buildRTC.bat
@@ -6,9 +6,14 @@ set ANT_HOME=%ECLIPSE_HOME%\plugins\org.apache.ant_1.7.0.v200803061910\
 
 cd jp.go.aist.rtm.RTC
 call ant buildAll -lib %ECLIPSE_HOME%\plugins\net.sf.ant4eclipse.plugin_0.5.0.rc1\lib\
+if not ERRORLEVEL 1 goto error_end 
 cd ..
-
-
 set ANT_HOME=%DUMMY%
+exit /b 0
+
+error_end:
+cd ..
+set ANT_HOME=%DUMMY%
+exit /b 1
 
 
