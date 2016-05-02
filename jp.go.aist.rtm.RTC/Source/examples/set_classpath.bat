90b7a9f8cd216faea35f059cc39367d2f445dc2d
kawauchi <openrtm@openrtm.org>
2016-05-02 07:13:34 +0000

    [compat,->RELENG_1_1] RTM_JAVA_ROOT has been modified to read in 'call set'.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/trunk@815 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/Source/examples/set_classpath.bat
+++ jp.go.aist.rtm.RTC/Source/examples/set_classpath.bat
@@ -1,5 +1,5 @@
-@echo off
-set JAR_BASE=%RTM_JAVA_ROOT%\jar\
-for /F %%A in ('dir "%JAR_BASE%OpenRTM*" /B') do (set FILE1=%%A)
-for /F %%A in ('dir "%JAR_BASE%commons-cli*" /B') do (set FILE2=%%A)
-set CLASSPATH=.;%JAR_BASE%%FILE1%;%JAR_BASE%%FILE2%
+@call set rtm_java_root=%RTM_JAVA_ROOT%
+@set JAR_BASE=%rtm_java_root%\jar\
+@for /F %%A in ('dir "%JAR_BASE%OpenRTM*" /B') do (set FILE1=%%A)
+@for /F %%A in ('dir "%JAR_BASE%commons-cli*" /B') do (set FILE2=%%A)
+@set CLASSPATH=.;%JAR_BASE%%FILE1%;%JAR_BASE%%FILE2%
