633a65ac5f3689aef3d91311ebbc21583eb6637c
win-ei <openrtm@openrtm.org>
2016-10-07 02:52:10 +0000

    Added components for tests of \"port connect function of a topic base\". refs #3391
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/trunk@839 9a6ac7ee-f497-465d-b832-7029f4913c1d

---
+++ jp.go.aist.rtm.RTC/installer/resources/Source/examples/TopicConssoleOut.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if test "x$RTM_JAVA_ROOT" = "x" ; then
+    echo "Environment variable RTM_JAVA_ROOT is not set."
+    echo "Please specify the OpenRTM-aist installation directory."
+    echo "Abort."
+    exit 1
+fi
+
+. ./search_classpath.func
+export CLASSPATH=`get_classpath`
+java RTMExamples.TopicTest.ConsoleOutComp -f RTMExamples/TopicTest/rtc.conf ${1+"$@"}
