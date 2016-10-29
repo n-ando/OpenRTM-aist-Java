d468eedd5235a6fe9bc7f2c89deabaed11b88243
win-ei <openrtm@openrtm.org>
2016-10-29 06:09:20 +0000

    Added JNA libraries to the classpath. refs #3395
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/trunk@865 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/installer/resources/Source/examples/set_classpath.bat
+++ jp.go.aist.rtm.RTC/installer/resources/Source/examples/set_classpath.bat
@@ -2,4 +2,7 @@
 @set JAR_BASE=%rtm_java_root%\jar\
 @for /F %%A in ('dir "%JAR_BASE%OpenRTM*" /B') do (set FILE1=%%A)
 @for /F %%A in ('dir "%JAR_BASE%commons-cli*" /B') do (set FILE2=%%A)
-@set CLASSPATH=.;%JAR_BASE%%FILE1%;%JAR_BASE%%FILE2%
+@for /F %%A in ('dir "%JAR_BASE%jna-?.?.?.*" /B') do (set FILE3=%%A)
+@for /F %%A in ('dir "%JAR_BASE%jna-platform-?.?.?.*" /B') do (set FILE4=%%A)
+@set CLASSPATH=.;%JAR_BASE%%FILE1%;%JAR_BASE%%FILE2%;%JAR_BASE%%FILE3%;%JAR_BASE%%FILE4%
+
