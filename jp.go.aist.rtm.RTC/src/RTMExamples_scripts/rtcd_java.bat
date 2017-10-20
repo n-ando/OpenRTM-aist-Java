817cf230aabc441a35e2d92a93a2ed42abf1d746
t-katami <openrtm@openrtm.org>
2017-10-20 01:34:17 +0000

    [compat,bugfix->releng_1_2] Eliminated reading of the configuration file. refs #4272
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/trunk@1059 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/RTMExamples_scripts/rtcd_java.bat
+++ jp.go.aist.rtm.RTC/src/RTMExamples_scripts/rtcd_java.bat
@@ -2,6 +2,5 @@
 
 call set_classpath.bat
 set CLASSPATH=%CLASSPATH%;%RTM_JAVA_ROOT%\jar\rtcd.jar;
-rem java -jar "%RTM_JAVA_ROOT%\jar\rtcd.jar" -f "%RTM_JAVA_ROOT%\bin\rtcd_java.conf" %*
-java rtcd.rtcd  -f "%RTM_JAVA_ROOT%\bin\rtcd_java.conf" %*
+java rtcd.rtcd  %*
 
