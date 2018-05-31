337328d8d89da16591f71ce4e5c7bf292449c83e
kawauchi <openrtm@openrtm.org>
2018-05-31 02:57:40 +0000

    [merge] r1132 have been merged from branches/RELENG_1_2.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Java/trunk@1136 9a6ac7ee-f497-465d-b832-7029f4913c1d

--- jp.go.aist.rtm.RTC/src/RTMExamples_scripts/rtcd_java.sh
+++ jp.go.aist.rtm.RTC/src/RTMExamples_scripts/rtcd_java.sh
@@ -26,6 +26,8 @@ get_classpath()
   echo ${class_path}
 }
 
+rtm_jar_path=`find /usr/lib -name OpenRTM-aist*.jar 2>/dev/null`
+RTM_JAVA_ROOT=`echo ${rtm_jar_path%/jar/*}`
 
 if test "x${RTM_JAVA_ROOT}" = "x" ; then
     echo "Environment variable RTM_JAVA_ROOT is not set."
